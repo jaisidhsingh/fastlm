e154f4aaa54cda9f7ad192943fff5d72a8c1b4ae
jaisidhsingh <jaisidhsingh@gmail.com>
2026-04-10 14:37:07 +0200

    write test for lm forward pass

--- models/transformer.py
+++ models/transformer.py
@@ -1,6 +1,7 @@
 """Transformer++, a simple LLama-style Transformer, supporting RMSNorm, RoPE, GLU"""
 
 import math
+import typing as tp
 from dataclasses import dataclass
 
 import torch
@@ -24,15 +25,24 @@ class ModelConfig:
   mlp: str = 'mlp'
   rmsnorm_eps: float = 1e-6
   tie_embeddings: bool = False
+
   token_mixer: str = 'gdn+attn'
-  hybrid_mixer_ratio: int = 3
+  hybrid_mixer_ratio = 3
+  # means 3:1 (one attention after every 3 gdn layers, type before the + symbol is the "every ratio layers")
   layer_norm_scaling: bool = False
+
+  attn_gate: bool = True
+  attn_qk_norm: bool = True
+
+  gdn_conv_size: int = 4
+  gdn_gate: bool = True
+  gdn_neg_eigval: bool = True
 
 
 MLP_CLASSES = {'mlp': MLP, 'glu': GLU, 'mlp_relu_sq': MLPReluSquared}
 
 
-class Attention(nn.Module):
+class GatedAttention(nn.Module):
   def __init__(self, cfg: ModelConfig):
     super().__init__()
     assert cfg.dim % cfg.n_heads == 0
@@ -42,7 +52,16 @@ class Attention(nn.Module):
     self.w_qkv = nn.Linear(cfg.dim, 3 * cfg.dim, bias=False)
     self.w_out = nn.Linear(cfg.dim, cfg.dim, bias=False)
 
-  def forward(self, x, freqs_cis, attn_mask=None):
+    self.use_gate = cfg.attn_gate
+    self.qk_norm = cfg.attn_qk_norm
+
+    if self.use_gate:
+      self.w_gate = nn.Linear(cfg.dim, cfg.dim, bias=False)
+    if self.qk_norm:
+      self.q_norm = RMSNorm(cfg.dim, cfg.rmsnorm_eps)
+      self.k_norm = RMSNorm(cfg.dim, cfg.rmsnorm_eps)
+
+  def forward(self, x, freqs_cis: torch.Tensor | None, attention_mask: torch.Tensor | None = None):
     bsz, seqlen, d = x.shape  # (bsz, seqlen, d)
 
     q, k, v = self.w_qkv(x).split(d, dim=2)  # (bsz, seqlen, d)
@@ -50,7 +69,12 @@ class Attention(nn.Module):
     k = k.view(bsz, seqlen, self.n_heads, self.head_dim)  # (bsz, seqlen, nh, h_dim)
     v = v.view(bsz, seqlen, self.n_heads, self.head_dim)  # (bsz, seqlen, nh, h_dim)
 
-    q, k = apply_rotary_emb_complex_like(q, k, freqs_cis=freqs_cis)  # (bsz, seqlen, nh, h_dim)
+    if self.qk_norm:
+      q = self.q_norm(q)
+      k = self.k_norm(k)
+
+    if freqs_cis is not None:
+      q, k = apply_rotary_emb_complex_like(q, k, freqs_cis=freqs_cis)  # (bsz, seqlen, nh, h_dim)
 
     q = q.transpose(1, 2)  # (bsz, nh, seqlen, h_dim)
     k = k.transpose(1, 2)  # (bsz, nh, seqlen, h_dim)
@@ -65,31 +89,56 @@ class Attention(nn.Module):
       attn_mask = attn_mask.unsqueeze(1)
       # pdb.set_trace()
 
-      out = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask)  # (bsz, nh, seqlen, h_dim)
+      out = F.scaled_dot_product_attention(q, k, v, attn_mask=attention_mask)  # (bsz, nh, seqlen, h_dim)
     else:
       out = F.scaled_dot_product_attention(q, k, v, is_causal=True)  # (bsz, nh, seqlen, h_dim)
 
     out = out.transpose(1, 2).contiguous().view(bsz, seqlen, d)  # (bsz, seqlen, d)
+    if self.use_gate:
+      gating = torch.sigmoid(self.w_gate(x))
+      out = out * gating
 
     return self.w_out(out)
 
 
-TOKEN_MIXERS = {'gdn': GatedDeltaNet, 'attn': Attention}
+SUPPORTED_TOKEN_MIXERS = {'gdn': 'gated_deltanet', 'attn': 'gated_attention'}
 
 
 class Block(nn.Module):
   def __init__(self, layer_id: int, token_mixer_type: str, cfg: ModelConfig):
-    super().__init__()
-    self.attn = TOKENIZER_MIXERS[token_mixer_type](cfg)
-    self.attn_norm = RMSNorm(cfg.dim, cfg.rmsnorm_eps)
+    assert token_mixer_type in SUPPORTED_TOKEN_MIXERS, 'Input token mixer is not supported'
+
+    if token_mixer_type == 'attn':
+      self.token_mixer = GatedAttention(cfg)
+    elif token_mixer_type == 'gdn':
+      self.token_mixer = GatedDeltaNet(
+        hidden_size=cfg.dim,
+        num_heads=cfg.n_heads,
+        head_dim=cfg.dim // cfg.n_heads,
+        allow_neg_eigval=cfg.gdn_neg_eigval,
+        use_gate=cfg.gdn_gate,
+        conv_size=cfg.gdn_conv_size,
+      )
+
+    self.token_mixer_norm = RMSNorm(cfg.dim, cfg.rmsnorm_eps)
     self.mlp = MLP_CLASSES[cfg.mlp](dim=cfg.dim, hidden_dim=int(cfg.expand * cfg.dim))
     self.mlp_norm = RMSNorm(cfg.dim, cfg.rmsnorm_eps)
-    self.layer_id = layer_id
 
-  def forward(self, x, freqs_cis, attn_mask):
+    self.layer_id = layer_id
+    self.layer_norm_scaling = cfg.layer_norm_scaling
+
+  def forward(self, x, freqs_cis, attention_mask):
     # x: (bsz, seqlen, dim)
-    x = x + self.attn(self.attn_norm(x), freqs_cis, attn_mask)
-    x = x + self.mlp(self.mlp_norm(x))
+    scaling = 1.0 if not self.layer_norm_scaling else math.sqrt(self.layer_id + 1)
+
+    x = scaling * self.token_mixer(x)
+    if isinstance(self.token_mixer, GatedAttention):
+      x = x + self.token_mixer(x, freqs_cis, attention_mask)
+    elif isinstance(self.token_mixer, GatedDeltaNet):
+      x = x + self.token_mixer(hidden_states=x, attention_mask=attention_mask)
+
+    x = scaling * self.mlp_norm(x)
+    x = x + self.mlp(x)
     return x
 
 
@@ -109,8 +158,8 @@ class Transformer(nn.Module):
     self.freqs_cis = precompute_freqs_cis(head_dim, cfg.seq_len, 500000)[0 : cfg.seq_len]
 
     # init all weights, scale residual branches
-    self.apply(self._init_weights)
-    self._scale_residual_branches()
+    # self.apply(self._init_weights)
+    # self._scale_residual_branches()
 
     if cfg.tie_embeddings:
       self.tie_weights()
@@ -124,7 +173,7 @@ class Transformer(nn.Module):
       )
       layers = []
       for idx in range(cfg.n_layers):
-        if idx != 0 and idx % cfg.hybrid_mixer_ratio == 0:
+        if (idx + 1) % (cfg.hybrid_mixer_ratio + 1) == 0:
           token_mixer_type = token_mixers[-1]
         else:
           token_mixer_type = token_mixers[0]
