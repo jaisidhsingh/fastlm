65119edf2a08370026a4994a4d5b1d07eac6488f
jaisidhsingh <jaisidhsingh@gmail.com>
2026-05-26 11:55:37 +0200

    move scripts and tokenize more

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
 mkdir -p /fast/jsingh/tmp
 
-# tokenize already download dataset (only to 1B tokens)
-PYTHONPATH=. python3 src/data/datasets/prepare.py \
+# tokenize already downloaded dataset (subsample of ~50B tokens)
+PYTHONPATH=/home/jsingh/projects/fastlm python3 src/data/datasets/prepare.py \
   --out_path="/fast/jsingh/data/nemotron-cc-sample-mtsynth" \
   --cache_path="/fast/jsingh/tmp" \
   --tokenize --chunk \
-  --nrows_tokenize=1000000 \
+  --nrows_tokenize=75000000 \
   --save_tokenized --save_tokenizer \
-  --seq_length=4096 --split_train_valid \
+  --seq_length=2048 --split_train_valid \
   --n_tokens_valid=10000000 \
   --dataset_path="MultiSynt/Nemotron-CC-sample-2" \
   --dataset_split="train"
+
---
+++ src/data/datasets/test.py
@@ -0,0 +1,3 @@
+from src.data.datasets.data_prep_utils import concat_chunck
+
+print("yes")
