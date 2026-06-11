a851619b143f3706f5afeaf3514d1eb5d3bc65a8
jaisidhsingh <jaisidhsingh@gmail.com>
2026-06-11 09:47:55 +0200

    finish gbs=16,32 main for all N

--- scripts/submit_jobs.sh
+++ scripts/submit_jobs.sh
@@ -3,10 +3,10 @@
 cd /home/jsingh/projects/fastlm
 
 arch_id="attn"
-n="50M"
+n="300M"
 gbs=32
 lr="all_parallel"
-mode="decay"
+mode="main"
 bid=1000
 
 python -m manager --arch_id=$arch_id --n=$n --gbs=$gbs --lr=$lr --mode=$mode --bid=$bid
