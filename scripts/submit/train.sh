bbc55452dade99bc52141ac688ae9fb8a49d6004
jasi149i <jasi149i@c1.capella.hpc.tu-dresden.de>
2026-07-22 07:53:33 +0200

    rho=0,1 N=150M,300M GBS=128 main on capella

--- scripts/submit/train.sh
+++ scripts/submit/train.sh
@@ -27,9 +27,9 @@ echo $CLUSTER_ID $PROJECT
 
 cd $PROJECT
 
-arch_id=("gdn+attn_1-1" "gdn+attn_1-3")
+arch_id=("gdn" "attn")
 n=("150M" "300M")
-gbs=(64)
+gbs=(128 256)
 lr="all_parallel"
 mode="main"
 bid=100
