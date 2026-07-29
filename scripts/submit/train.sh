0eed4316c70795df49a09663380209c06438a81d
jaisidhsingh <jaisidhsingh@gmail.com>
2026-07-29 17:53:32 +0200

    rho=0.25,0.5m0.75 N=20M,50M GBS=16 main on mpi

--- scripts/submit/train.sh
+++ scripts/submit/train.sh
@@ -27,9 +27,9 @@ echo $CLUSTER_ID $PROJECT
 
 cd $PROJECT
 
-arch_id=("gdn+attn_3-1")
+arch_id=("gdn+attn_1-3" "gdn+attn_1-1" "gdn+attn_3-1")
 n=("20M" "50M")
-gbs=(128 256)
+gbs=(16)
 lr="all_parallel"
 mode="main"
 bid=100
