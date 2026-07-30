012c865a0d1574d04c5f5c08cd6814b2b96b4f88
jasi149i <jasi149i@login1.alpha.hpc.tu-dresden.de>
2026-07-30 17:40:49 +0200

    rho=0.75 N=20M,50M GBS=128,256 decay on alpha

--- scripts/submit/train.sh
+++ scripts/submit/train.sh
@@ -27,9 +27,9 @@ echo $CLUSTER_ID $PROJECT
 
 cd $PROJECT
 
-arch_id=("gdn+attn_1-3" "gdn+attn_1-1" "gdn+attn_3-1")
+arch_id=("gdn+attn_3-1")
 n=("20M" "50M")
-gbs=(16)
+gbs=(128 256)
 lr="all_parallel"
 mode="decay"
 bid=100
