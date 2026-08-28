e2e15dd8040bf0be3f1ad16ebe0d2e6b2ac976fa
jaisidhsingh <jaisidhsingh@gmail.com>
2026-08-28 17:55:25 +0200

    test out new backend model init. (finish later, tired rn)

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,7 @@
-from .construct import *
-from .registration import *
+from .legacy.construct import *
+from .legacy.registration import *
+from .builder import config_builder 
 
-__all__ = ['construct_model', 'get_param_groups', 'construct_model_config']
+
+__all__ = ['construct_model', 'get_param_groups', 'construct_model_config', 'config_builder']
+
---
+++ testing/new_backend.py
@@ -0,0 +1,54 @@
+import tyro
+from types import SimpleNamespace
+import torch
+import yaml
+from transformers import AutoModelForCausalLM
+from dataclasses import dataclass
+from src.models import config_builder
+
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu" 
+MODEL_DTYPE = torch.bfloat16
+
+
+@dataclass
+class TestingConfig:
+  batch_size: int = 2
+  seq_len: int = 128
+  dim: int = 32
+  test_config: str = "/home/jsingh/projects/fastlm/src/configs/int/attn_300M.yaml"
+
+
+def test_new_backend_config_init(cfg):
+  model_config = config_builder(cfg)
+  print(model_config)
+
+
+def test_new_backend_model_init(cfg): 
+  model_config = config_builder(cfg)
+  model = AutoModelForCausalLM.from_config(model_config)
+  model = model.to(dtype=MODEL_DTYPE, device=DEVICE)
+  print(model)
+  return model, model_config
+
+
+@torch.inference_mode()
+def test_new_backend_forward_pass(cfg, test_cfg):
+  model, _ = test_new_backend_model_init(cfg)
+  
+  input_shape = (test_cfg.batch_size, test_cfg.seq_len, test_cfg.dim)
+  x = torch.ones(input_shape, dtype=torch.long, device=DEVICE)
+  y = model(x)
+  assert x.shape == y.shape
+
+
+def main(test_cfg):
+  with open(test_cfg.test_config) as f:
+    cfg = SimpleNamespace(**yaml.safe_load(f))
+
+  test_new_backend_forward_pass(cfg, test_cfg)
+
+if __name__ == "__main__":
+  test_cfg = tyro.cli(TestingConfig, default=TestingConfig())
+  main(test_cfg)
+
